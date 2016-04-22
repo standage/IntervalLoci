2187697d1c42f1ad7ca05be97e641645e654396b
Daniel Standage <daniel.standage@gmail.com>
2016-04-22 16:47:49 -0400

    Random procedure, and phi/sigma comparison of annotations vs random gene arrangements

---
+++ shuffle/README.md
@@ -0,0 +1,1 @@
+parallel --gnu --jobs=10 bash shuffle.sh {} ::: Scer Cele Crei Mtru Agam Dmel Xtro Drer Mmus Hsap
--- shuffle/shuffle.sh
+++ shuffle/shuffle.sh
@@ -4,3 +4,7 @@ locuspocus --verbose --delta=0 --skipends --outfile=species/${species}/${species
 ./shuffle_iloci.py --seed=54321 --out=species/${species}/${species}.shuffled.genes.gff3 species/${species}/${species}.iloci.0d.gff3
 locuspocus --verbose --delta=500 --skipends --cds --namefmt="${species}ILCshuf-%06lu" --outfile=species/${species}/${species}.iloci.shuffled.gff3 species/${species}/${species}.shuffled.genes.gff3
 miloci.py < species/${species}/${species}.iloci.shuffled.gff3 > species/${species}/${species}.miloci.shuffled.gff3
+xtractore --type=locus --outfile=species/${species}/${species}.iloci.shuffled.fa species/${species}/${species}.iloci.shuffled.gff3 species/${species}/${species}.gdna.fa
+xtractore --type=locus --outfile=species/${species}/${species}.miloci.shuffled.fa species/${species}/${species}.miloci.shuffled.gff3 species/${species}/${species}.gdna.fa
+genhub-stats.py --species=${species} --iloci species/${species}/${species}.iloci.shuffled.gff3 species/${species}/${species}.iloci.shuffled.fa species/${species}/${species}.iloci.shuffled.tsv
+genhub-stats.py --species=${species} --miloci species/${species}/${species}.miloci.shuffled.gff3 species/${species}/${species}.miloci.shuffled.fa species/${species}/${species}.miloci.shuffled.tsv
